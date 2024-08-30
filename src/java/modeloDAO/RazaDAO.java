00650d6656d0e94d4d4c51a574e39c4c4ad142cb
Jesus <jgalviscontreras38@gmail.com>
2024-08-29 22:24:27 -0500

    olvide algunas cosas de raza

--- src/java/modeloDAO/RazaDAO.java
+++ src/java/modeloDAO/RazaDAO.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modeloDAO;
 
-/**
- *
- * @author hp
- */
-public class RazaDAO {
+import config.conexion;
+import interfaces.raza;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Raza;
+
+
+public class RazaDAO implements raza{
+    
+    conexion cn = new conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    
+    @Override
+    public List<Raza> listar() {
+        List<Raza> list = new ArrayList<>();
+//        asignamos a una variable la consulta
+        String sql = "SELECT * FROM tipo_raza";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Raza rol = new Raza();
+                rol.setId(rs.getInt("id"));
+                rol.setNombre(rs.getString("nombre"));
+//                agregamos los datos de los seter y geter a list(instancia de modelo)
+                list.add(rol);
+            }
+        } catch (Exception e) {
+            System.err.println("Error al listar lotes: " + e);
+        } 
+        return list;
+    }
+
+    @Override
+    public Raza list(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(Raza raz) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean edit(Raza raz) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
